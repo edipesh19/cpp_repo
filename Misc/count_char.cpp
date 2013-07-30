dc806843fb2066185e9cc43ee488d869ef15e29a
Dipesh Kumar Dutta <dipeshkumar.dutta@gmail.com>
2013-07-30 15:35:16 +0530

    Mmoved to CPP

--- Misc/count_char.cpp
+++
@@ -1,9 +0,0 @@
-#include <algorithm>
-#include <iostream>
-//unsing namespace std;
-int main()
-{
-    std::string s = "a_b_c";
-    int i = std::count(s.begin(), s.end(), '_');
-    std::cout<<"i :"<<i<<std::endl;;
-}
